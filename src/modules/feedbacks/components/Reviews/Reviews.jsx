cf8cade954af5a2f7b1f3206794aa6b514d28332
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-25 12:27:47 +0200

    fixed reviews comp/added swiper

--- src/modules/feedbacks/components/Reviews/Reviews.jsx
+++ src/modules/feedbacks/components/Reviews/Reviews.jsx
@@ -1,27 +1,30 @@
-import s from './Reviews.module.scss';
+// import s from './Reviews.module.scss';
 import ReviewCard from '../ReviewCard/ReviewCard';
 import { reviewData } from '../../service/service.js';
-// import Swiper from 'swiper';
-// import { Navigation, Pagination } from 'swiper/modules'
-// import 'swiper/css'
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-
-// const swiper = new Swiper('.swiper', {
-//   modules: [Navigation, Pagination]
-// })
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const Reviews = () => {
   return (
-    <ul className={s.list}>
+    <Swiper
+      modules={[Navigation, Pagination]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: false }}
+    >
       {reviewData.map((review) => {
         return (
-          <li key={review.id}>
+          <SwiperSlide key={review.id}>
             <ReviewCard review={review} />
-          </li>
+          </SwiperSlide>
         );
       })}
-    </ul>
+    </Swiper>
   );
 };
 
