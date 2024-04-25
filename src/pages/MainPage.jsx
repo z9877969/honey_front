c33ab6cd9105f77c783561c6749301a4e176ff2a
DrunkCherry1997 <poisonivy1997@ukr.net>
2024-04-25 11:16:57 +0200

    add hrml scelet and few scss styles

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,9 +1,11 @@
 import { MainPageHero } from 'modules/mainPageHero';
+import PopUpInfoProduct from '../modules/mainPageHero/components/MainPageHero/PopUpInfoProduct/PopUpInfoProduct';
 
 const MainPage = () => {
   return (
     <>
       <MainPageHero />
+      <PopUpInfoProduct />
     </>
   );
 };
