14bc8fbeb50033375dc2c63a549d10907e04f521
Kristina <kr.hranovska@gmail.com>
2024-04-25 15:25:27 +0300

    add img

--- src/modules/ourProducts/data/dbProducts.js
+++ src/modules/ourProducts/data/dbProducts.js
@@ -1,27 +1,39 @@
-export const dbProducts = [
+import { pollen } from "../images";
+import { cashewHoney } from "../images";
+import { honeyWithAlmonds } from "../images";
+import { honeyWithHazelnuts } from "../images";
+import { honeyWithNutsMix } from "../images";
+import { honeyWithWalnuts } from "../images";
+import { honeycombs } from "../images";
+import { naturalAcaciaHoney } from "../images";
+import { naturalFloralHoney } from "../images";
+import { naturalRapeseedHoney } from "../images";
+import { naturalSunflowerHoney } from "../images";
+
+const dbProducts = [
     {
-        img: 'link',
+        img: naturalAcaciaHoney,
         name: 'Мед акацієвий натуральний',
         weight: '0.25-3.0 л',
         price: '100 грн - 1000 грн',
         category: 'honey'
     },
     {
-        img: 'link',
+        img: naturalFloralHoney,
         name: 'Мед квітковий натуральний',
         weight: '0.25-3.0 л',
         price: '75 грн - 800 грн',
         category: 'honey'
     },
     {
-        img: 'link',
+        img: naturalRapeseedHoney,
         name: 'Мед ріпаковий натуральний',
         weight: '0.25-3.0 л',
         price: '50 грн - 550 грн',
         category: 'honey'
     },
     {
-        img: 'link',
+        img: naturalSunflowerHoney,
         name: 'Мед соняшниковий натуральний',
         weight: '0.25-3.0 л',
         price: '50 грн - 550 грн',
@@ -29,35 +41,35 @@ export const dbProducts = [
     },
 
     {
-        img: 'link',
+        img: honeyWithWalnuts,
         name: 'Мед з волоськимим горіхами',
         weight: '250 мл',
         price: '200 грн',
         category: 'honey with nuts'
     },
     {
-        img: 'link',
+        img: honeyWithAlmonds,
         name: 'Мед з мигдалем',
         weight: '250 мл',
         price: '200 грн',
         category: 'honey with nuts'
     },
     {
-        img: 'link',
+        img: cashewHoney,
         name: "Мед з кеш'ю",
         weight: '250 мл',
         price: '200 грн',
         category: 'honey with nuts'
     },
     {
-        img: 'link',
+        img: honeyWithHazelnuts,
         name: 'Мед з ліщиною',
         weight: '250 мл',
         price: '200 грн',
         category: 'honey with nuts'
     },
     {
-        img: 'link',
+        img: honeyWithNutsMix,
         name: 'Мед з горіхами мікс',
         weight: '250 мл',
         price: '200 грн',
@@ -65,17 +77,19 @@ export const dbProducts = [
     },
 
     {
-        img: 'link',
+        img: honeycombs,
         name: 'Стільники з акацієвим медом',
         weight: '100 мл',
         price: '75 грн',
         category: 'honeycombs'
     },
     {
-        img: 'link',
+        img: pollen,
         name: 'Пилок',
         weight: '100 мл',
         price: '50 грн',
         category: 'pollen'
-    },
+    }
 ];
+
+export default dbProducts;
