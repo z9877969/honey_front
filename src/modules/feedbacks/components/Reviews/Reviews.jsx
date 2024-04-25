f2e8fd4a1616925e4aa8344651d7125b4786eb9f
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-25 20:06:25 +0200

    fixed review comps

--- src/modules/feedbacks/components/Reviews/Reviews.jsx
+++ src/modules/feedbacks/components/Reviews/Reviews.jsx
@@ -1,32 +1,34 @@
 import s from './Reviews.module.scss';
 import ReviewCard from '../ReviewCard/ReviewCard';
+import { Container, MainTitle } from 'shared/components';
 import { data } from '../../data/data.js';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
 import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const Reviews = () => {
   return (
-    <section className={s.section}>
-      <h2 className={s.sectionHeader}>Відгуки покупців</h2>
-      <Swiper
-        modules={[Navigation, Pagination]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: false }}
-      >
-        {data.map((data) => {
-          return (
-            <SwiperSlide key={data.id}>
-              <ReviewCard data={data} />
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+    <section>
+      <Container className={s.section}>
+        <MainTitle title={'Відгуки покупців'} className={s.sectionHeader} />
+        <Swiper
+          modules={[Navigation, Pagination]}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: false }}
+        >
+          {data.map((data) => {
+            return (
+              <SwiperSlide key={data.id}>
+                <ReviewCard data={data} />
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      </Container>
     </section>
   );
 };
