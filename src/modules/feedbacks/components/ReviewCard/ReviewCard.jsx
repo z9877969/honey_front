93f5d8e3243b13762180ef03e18217a0141b8f0e
Zenoviy Fil <98259328+zenoviyfil@users.noreply.github.com>
2024-04-26 18:43:13 +0200

    fixed review comp css polygon/photo

--- src/modules/feedbacks/components/ReviewCard/ReviewCard.jsx
+++ src/modules/feedbacks/components/ReviewCard/ReviewCard.jsx
@@ -1,9 +1,18 @@
 import s from './ReviewCard.module.scss';
+import sprite from '../../../../shared/icons/sprite.svg';
 
 const ReviewCard = ({ data: { photo, name, review } }) => {
   return (
     <div className={s.card}>
-      <img className={s.photo} src={photo} alt="photo" />
+      <div className={s.photoContainer}>
+        <svg width={190} height={172}>
+          <use
+            xlinkHref={`${sprite}#polygon`}
+            className={s.iconClassName}
+          ></use>
+        </svg>
+        <img className={s.photo} src={photo} alt="photo" />
+      </div>
       <div className={s.content}>
         <h3 className={s.name}>{name}</h3>
         <p className={s.review}>{review}</p>
