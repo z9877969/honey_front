a9e4190c1ddf23e7ad232241680fe2d6978781c8
culplate <culplate@gmail.com>
2024-04-26 21:17:57 +0200

    Footer contacts styles

--- src/modules/footer/components/Footer/Footer.jsx
+++ src/modules/footer/components/Footer/Footer.jsx
@@ -1,28 +1,28 @@
 import { Container, Logo } from 'shared/components';
 import s from './Footer.module.scss';
 import SocialIcons from '../SocialIcons/SocialIcons';
+import Contacts from '../Contacts/Contacts';
 
 const Footer = () => {
   return (
     <footer className={s.footer}>
-      <Container>
+      <Container className={s.container}>
         <div className={s.contentWrap}>
           <Logo
             id={'logo'}
             iconClassName={s.logoW}
             iconHeight={24}
             iconWidth={24}
-            linkClassName={s.link}
+            linkClassName={s.logo}
           />
           <div className={s.socialWrap}>
-            <ul className={s.contacts}>
-              <li>plokhaanastasiia@gmail.com</li>
-              <li>+38 096 888 07 39</li>
-            </ul>
+            <Contacts />
             <SocialIcons />
           </div>
         </div>
-        <button type="button">Created by GoIT</button>
+        <button type="button" className={s.footBtn}>
+          Created by GoIT
+        </button>
       </Container>
     </footer>
   );
