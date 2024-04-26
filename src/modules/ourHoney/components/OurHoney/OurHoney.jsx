d5489af916e015f7890e6dd54c50c069a8bb64b9
Oleksiy Zhuravlev <zhouravlevav@gmail.com>
2024-04-26 18:05:25 +0300

    to review

--- src/modules/ourHoney/components/OurHoney/OurHoney.jsx
+++ src/modules/ourHoney/components/OurHoney/OurHoney.jsx
@@ -1,52 +1,35 @@
 import { Container, MainTitle } from 'shared/components';
 
-import leaves from '../../images/leaves.svg';
-import honeyCan from '../../images/honeyCan.svg';
-import happyBox from '../../images/happyBox.svg';
+import ourHoneySet from '../../images/ourHoneySet.svg';
 
 import s from './OurHoney.module.scss';
+import HoneyFeature from '../HoneyFeature/HoneyFeature';
 const OurHoney = () => {
   return (
-    <Container>
-      <MainTitle title={'Наш мед це'} className={s.honeyTitle} />
-      <div className={s.honeyArea}>
-        <div className={s.honeyFeatureThumb}>
-          <div className={s.sotaArea}>
-            <img src={leaves} width="43.73" height="48" alt="leaves" />
-          </div>
-          <div className={s.honeyText}>
-            <p className={s.firstLine}>Цілющі властивості</p>
-            <p className={s.secondLine}>
-              Наш мед зібраний з цвітінь кращих полів Подніпровʼя.{' '}
-            </p>
-          </div>
+    <section className={s.section}>
+      <Container className={s.container}>
+        <MainTitle title={'Наш мед це'} className={s.honeyTitle} />
+        <div className={s.honeyArea}>
+          <HoneyFeature
+            svg={`${ourHoneySet}#leaves`}
+            firstLine={'Цілющі властивості'}
+            secondLine={'Наш мед зібраний з цвітінь кращих полів Подніпровʼя.'}
+          />
+          <HoneyFeature
+            svg={`${ourHoneySet}#honeyCan`}
+            firstLine={'Родинні традиції'}
+            secondLine={'Зберігає традиції та має свою сімейну історію.'}
+          />
+          <HoneyFeature
+            svg={`${ourHoneySet}#happyBox`}
+            firstLine={'Чудовий подарунок'}
+            secondLine={
+              'Для близьких людей, адже цей мед про турботу та любов!'
+            }
+          />
         </div>
-
-        <div className={s.honeyFeatureThumb}>
-          <div className={s.sotaArea}>
-            <img src={honeyCan} width="43.73" height="48" alt="Honey can" />
-          </div>
-          <div className={s.honeyText}>
-            <p className={s.firstLine}>Родинні традиції</p>
-            <p className={s.secondLine}>
-              Зберігає традиції та має свою сімейну історію.
-            </p>
-          </div>
-        </div>
-
-        <div className={s.honeyFeatureThumb}>
-          <div className={s.sotaArea}>
-            <img src={happyBox} width="43.73" height="48" alt="Honey can" />
-          </div>
-          <div className={s.honeyText}>
-            <p className={s.firstLine}>Чудовий подарунок</p>
-            <p className={s.secondLine}>
-              Для близьких людей, адже цей мед про турботу та любов!
-            </p>
-          </div>
-        </div>
-      </div>
-    </Container>
+      </Container>
+    </section>
   );
 };
 
