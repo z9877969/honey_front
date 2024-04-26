faa09d41499f9d05e1ccde4bc03f426416afbbae
Kristina <kr.hranovska@gmail.com>
2024-04-26 20:19:15 +0300

    section

--- src/shared/components/SectionMain/SectionMain.jsx
+++ src/shared/components/SectionMain/SectionMain.jsx
@@ -1,13 +1,9 @@
+import clsx from 'clsx';
 import s from './SectionMain.module.scss';
 
-const SectionMain = ({ children, section }) => {
-  const typeSection = ['hero', 'footer', 'main'];
-  const validSection = typeSection.includes(section) ? section : 'main';
-  const sectionClassName =
-    s[`section${validSection.charAt(0).toUpperCase()}${validSection.slice(1)}`];
-
+const SectionMain = ({ children, sectionStyles }) => {
   return (
-    <section className={`${s.sectionMain} ${sectionClassName}`}>
+    <section className={clsx(sectionStyles ? sectionStyles : s.sectionMain)}>
       {children}
     </section>
   );
