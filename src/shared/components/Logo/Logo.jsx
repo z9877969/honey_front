573a247cf494d4dcee21bca04fb4b85f0ba41073
Nazar Dia <nazar.dia@gmail.com>
2024-04-29 12:05:00 +0200

    Написав розмітку хедеру. Трохи ще стилі треба буде доробити.

--- src/shared/components/Logo/Logo.jsx
+++ src/shared/components/Logo/Logo.jsx
@@ -1,12 +1,13 @@
 import clsx from 'clsx';
 import s from './Logo.module.scss';
 import LogoText from '../LogoText/LogoText';
+import sprite from '../../icons/sprite.svg';
 
 const Logo = ({ className }) => {
   return (
     <a className={clsx(s.logo, className && className)} href="#">
-      <svg width="36" height="36">
-        <use href="../../icons/sprite.svg#logo" />
+      <svg className={s.icon} width="36" height="36">
+        <use xlinkHref={`${sprite}#logo`} />
       </svg>
       <LogoText title={'Бджола та Шершень'} />
     </a>
