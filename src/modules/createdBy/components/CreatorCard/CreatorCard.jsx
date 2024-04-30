a959c0b2f8354713bc4eae32d81fdbc08059c362
culplate <culplate@gmail.com>
2024-04-30 16:48:02 +0200

    styles and structure for creatorCard picture

--- src/modules/createdBy/components/CreatorCard/CreatorCard.jsx
+++ src/modules/createdBy/components/CreatorCard/CreatorCard.jsx
@@ -1,14 +1,22 @@
 import clsx from 'clsx';
 import s from './CreatorCard.module.scss';
-
+import cardEmptyPhoto from '../../images/cardPhoto_empty.png';
 const CreatorCard = ({ data }) => {
   return (
-    <li className={s.card}>
-      <img className={s.cardImg} src={data.picture} alt="" />
-      <div className={s.infoWrap}>
-        <p className={s.cardText}>{data.name}</p>
-        <p className={clsx(s.cardText, s.cardLightText)}>{data.position}</p>
-      </div>
+    <li>
+      <a href={data.link} className={s.card}>
+        <div className={s.cardImgWrap}>
+          <img
+            className={s.cardImg}
+            src={data.picture === '' ? cardEmptyPhoto : data.picture}
+            alt={`${data.name} photo`}
+          />
+        </div>
+        <div className={s.infoWrap}>
+          <p className={s.cardText}>{data.name}</p>
+          <p className={clsx(s.cardText, s.cardLightText)}>{data.position}</p>
+        </div>
+      </a>
     </li>
   );
 };
