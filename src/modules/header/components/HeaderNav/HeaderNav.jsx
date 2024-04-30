62342a3451414b296ada4276e531ccae5bc67276
Nazar Dia <nazar.dia@gmail.com>
2024-04-30 11:20:44 +0200

    edited header styles

--- src/modules/header/components/HeaderNav/HeaderNav.jsx
+++ src/modules/header/components/HeaderNav/HeaderNav.jsx
@@ -1,9 +1,12 @@
 import s from './HeaderNav.module.scss';
 import sprite from '../../../../shared/icons/sprite.svg';
 import { useState } from 'react';
+import { Cart } from 'modules/cart';
 
 const HeaderNav = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
+  // const [orderCount, setOrderCount] = useState(0);
+
   const toggleNav = () => {
     setIsNavOpen(!isNavOpen);
   };
@@ -11,44 +14,46 @@ const HeaderNav = () => {
   const closeNav = () => {
     setIsNavOpen(false);
   };
+  // const updateOrderCount = (count) => {
+  //   setOrderCount(count);
+  // };
 
   return (
-    <div>
+    <div className={s.desktopHeader}>
       <button onClick={toggleNav}>
         <svg className={s.burgerIcon}>
           <use xlinkHref={`${sprite}#menu`} />
         </svg>
       </button>
-      <button>
+      <ul className={`${s.navList} ${isNavOpen ? s.navListOpen : ''}`}>
+        <li className={s.navListItem}>
+          <a href="#ourProducts">Каталог</a>
+        </li>
+        <li className={s.navListItem}>
+          <a href="#aboutUS">Про нас</a>
+        </li>
+        <li className={s.navListItem}>
+          <a href="#reviews">Відгуки</a>
+        </li>
+        <li className={s.navListItem}>
+          <a href="#footer">Контакти</a>
+        </li>
+        <li>
+          {isNavOpen && (
+            <button onClick={closeNav}>
+              <svg className={s.closeIcon}>
+                <use xlinkHref={`${sprite}#cross-close`} />
+              </svg>
+            </button>
+          )}
+        </li>
+      </ul>
+      <button className={s.cartButton} onClick={Cart.isOpen}>
         <svg className={s.cartIcon}>
           <use xlinkHref={`${sprite}#basket`} />
         </svg>
+        <span className={s.orderCount}>{}</span>
       </button>
-      <div className={s.dropDown}>
-        <ul className={`${s.navList} ${isNavOpen ? s.navListOpen : ''}`}>
-          <li className={s.navListItem}>
-            <a href="#">Каталог</a>
-          </li>
-          <li className={s.navListItem}>
-            <a href="#">Про нас</a>
-          </li>
-          <li className={s.navListItem}>
-            <a href="#">Відгуки</a>
-          </li>
-          <li className={s.navListItem}>
-            <a href="#">Контакти</a>
-          </li>
-          <li>
-            {isNavOpen && (
-              <button onClick={closeNav}>
-                <svg className={s.closeIcon}>
-                  <use xlinkHref={`${sprite}#cross-close`} />
-                </svg>
-              </button>
-            )}
-          </li>
-        </ul>
-      </div>
     </div>
   );
 };
