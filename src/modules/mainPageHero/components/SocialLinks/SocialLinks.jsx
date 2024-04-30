9297c5386e10acd5a8ed8f4188220bbde8244c6f
Pavel <Pavel.Pasechnik@gmail.com>
2024-04-30 13:59:31 +0300

    delete noopener

--- src/modules/mainPageHero/components/SocialLinks/SocialLinks.jsx
+++ src/modules/mainPageHero/components/SocialLinks/SocialLinks.jsx
@@ -3,7 +3,7 @@ import s from './SocialLinks.module.scss';
 const SocialLinks = ({ icon1, icon2 }) => {
   return (
     <div className={s.social}>
-      <a className={s.link} href="#" target="blank" rel="noreferrer noopener">
+      <a className={s.link} href="#" target="blank" rel="noreferrer">
         <svg className={s.icon}>
           <use xlinkHref={icon1} />
         </svg>
@@ -12,7 +12,7 @@ const SocialLinks = ({ icon1, icon2 }) => {
         className={s.link}
         href="https://www.instagram.com/bdzhola_ta_shershen?igsh=MXY0YndodWYyM3JoYw%3D%3D&utm_source=qr"
         target="blank"
-        rel="noreferrer noopener"
+        rel="noreferrer"
       >
         <svg className={s.icon}>
           <use xlinkHref={icon2} />
