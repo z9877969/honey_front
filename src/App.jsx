78608d65f0a6c1d7418eda9cee73e2d409221f31
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-05-01 17:48:21 +0200

    select-scrollbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { MainPage } from './pages';
 import { SharedLayout } from 'shared/components';
-import { OrderCheckoutPage } from 'modules/order';
 
 function App() {
   return (
     <>
-      {/* <SharedLayout>
+      <SharedLayout>
         <MainPage />
-      </SharedLayout> */}
-      <OrderCheckoutPage />
+      </SharedLayout>
     </>
   );
 }
