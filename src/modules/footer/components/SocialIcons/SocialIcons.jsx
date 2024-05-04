82e6b1346f1cdaa66b8f0d495b3d58318068f9c1
culplate <culplate@gmail.com>
2024-05-04 14:14:15 +0200

    added :active selectors and target blank

--- src/modules/footer/components/SocialIcons/SocialIcons.jsx
+++ src/modules/footer/components/SocialIcons/SocialIcons.jsx
@@ -5,14 +5,14 @@ const SocialIcons = () => {
   return (
     <ul className={s.iconsList}>
       <li>
-        <a href="https://telegram.org/">
+        <a href="https://telegram.org/" target="_blank">
           <svg width={32} height={32} className={s.icon}>
             <use xlinkHref={`${sprite}#telegram`}></use>
           </svg>
         </a>
       </li>
       <li>
-        <a href="https://www.instagram.com/bdzhola_ta_shershen">
+        <a href="https://www.instagram.com/bdzhola_ta_shershen" target="_blank">
           <svg width={32} height={32} className={s.icon}>
             <use xlinkHref={`${sprite}#instagram`}></use>
           </svg>
