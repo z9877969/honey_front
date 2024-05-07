be9e1d76e51e9b36911bc02215baefbc868e7982
YevhenKharchenko <evgenvalerych@gmail.com>
2024-05-07 19:53:41 +0300

    added isOpen to the store

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import { useModal } from 'hooks/useModal';
+import { selectIsOpen } from '@redux/cart/cartSlice';
 import { MainPage } from './pages';
 import { SharedLayout } from 'shared/components';
+import { Cart } from 'modules/cart';
 
 function App() {
+  const isOpen = useSelector(selectIsOpen);
+  const setModal = useModal();
+
+  const closeModal = useCallback(() => {
+    setModal();
+  }, [setModal]);
+
+  const openModal = useCallback(() => {
+    setModal(<Cart onClose={closeModal} />);
+  }, [setModal, closeModal]);
+
+  useEffect(() => {
+    if (isOpen) {
+      openModal();
+    }
+  }, [isOpen, openModal]);
+
   return (
     <>
       <SharedLayout>
