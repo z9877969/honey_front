fb5a3b3d132fa54a52c6a30aac9aa50b5d5946bb
Zymbsis-fs <Oleksandra-fsdev@gmail.com>
2024-05-10 12:40:08 +0200

    min-fix

--- src/helpers/scrollToTop.js
+++ src/helpers/scrollToTop.js
@@ -1,10 +1,10 @@
 export const scrollToTop = (id) => {
-  const ourProducts = document.querySelector(id);
-  if (!ourProducts) return;
+  const getSection = document.querySelector(id);
+  if (!getSection) return;
   const isMobileView = window.innerWidth < 768;
   const isTabletView = window.innerWidth >= 768 && window.innerWidth < 1440;
   const topOffset = isMobileView ? 56 : isTabletView ? 72 : 76;
-  const topPosition = ourProducts.offsetTop - topOffset;
+  const topPosition = getSection.offsetTop - topOffset;
 
   window.scrollTo({
     top: topPosition,
