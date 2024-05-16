e2992ece9ccde71b064f16d5f9aa7113a45f7c49
Kristina <kr.hranovska@gmail.com>
2024-05-16 16:33:03 +0300

    add responsive

--- src/modules/ourProducts/components/ProductsList/ProductsList.jsx
+++ src/modules/ourProducts/components/ProductsList/ProductsList.jsx
@@ -1,16 +1,19 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import ProductItem from '../ProductsItem/ProductsItem';
 import style from './ProductsList.module.scss';
 import { icons } from 'shared/icons';
 import { PopUpDetailedInfo } from 'modules/ourProducts/';
 import { useModal } from 'hooks/useModal';
 
+import { useMediaQuery } from 'react-responsive';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 const ProductsList = ({ currentCategory, data }) => {
   const [swiper, setSwiper] = useState(null);
-  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 1440);
+
+  const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1439px)' });
+  const isDesctop = useMediaQuery({ query: '(min-width: 1440px)' });
 
   const setModal = useModal();
 
@@ -33,81 +36,70 @@ const ProductsList = ({ currentCategory, data }) => {
   );
   const showArrows = filteredProducts.length > 3;
 
-  useEffect(() => {
-    const handleResize = () => {
-      setIsDesktop(window.innerWidth >= 1440);
-    };
-
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  if (!isDesktop) {
-    return (
-      <>
-        {filteredProducts.length > 0 && (
-          <ul className={style.productsList}>
-            {filteredProducts.map((product) => (
-              <li
-                key={product.id}
-                className={style.productItem}
-                onClick={() => handleDetailedInfo(product)}
-              >
-                <ProductItem product={product} />
-              </li>
-            ))}
-          </ul>
-        )}
-      </>
-    );
-  }
-
   return (
     <>
-      <div className={style.thumbList}>
-        {showArrows && (
-          <button
-            className={style.prevButton}
-            onClick={() => swiper && swiper.slidePrev()}
-          >
-            <svg width="48" height="48">
-              <use xlinkHref={`${icons}#arrow-left`} />
-            </svg>
-          </button>
-        )}
+      {isTabletOrMobile && (
+        <>
+          {filteredProducts.length > 0 && (
+            <ul className={style.productsList}>
+              {filteredProducts.map((product) => (
+                <li
+                  key={product.id}
+                  className={style.productItem}
+                  onClick={() => handleDetailedInfo(product)}
+                >
+                  <ProductItem product={product} />
+                </li>
+              ))}
+            </ul>
+          )}
+        </>
+      )}
 
-        {filteredProducts.length > 0 && (
-          <Swiper
-            rewind={true}
-            onSwiper={setSwiper}
-            slidesPerView={3}
-            className={style.swiperWrapper}
-          >
-            {filteredProducts.map((product) => (
-              <SwiperSlide
-                key={product.id}
-                className={style.listDesctop}
-                onClick={() => handleDetailedInfo(product)}
-              >
-                <ProductItem product={product} />
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        )}
+      {isDesctop && (
+        <div className={style.thumbList}>
+          {showArrows && (
+            <button
+              className={style.prevButton}
+              onClick={() => swiper && swiper.slidePrev()}
+            >
+              <svg width="48" height="48">
+                <use xlinkHref={`${icons}#arrow-left`} />
+              </svg>
+            </button>
+          )}
 
-        {showArrows && (
-          <button
-            className={style.nextButton}
-            onClick={() => swiper && swiper.slideNext()}
-          >
-            <svg width="48" height="48">
-              <use xlinkHref={`${icons}#arrow-right`} />
-            </svg>
-          </button>
-        )}
-      </div>
+          {filteredProducts.length > 0 && (
+            <Swiper
+              rewind={true}
+              onSwiper={setSwiper}
+              slidesPerView={3}
+              className={style.swiperWrapper}
+            >
+              {filteredProducts.map((product) => (
+                <SwiperSlide
+                  key={product.id}
+                  className={style.listDesctop}
+                  onClick={() => handleDetailedInfo(product)}
+                >
+                  <ProductItem product={product} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          )}
+
+          {showArrows && (
+            <button
+              className={style.nextButton}
+              onClick={() => swiper && swiper.slideNext()}
+            >
+              <svg width="48" height="48">
+                <use xlinkHref={`${icons}#arrow-right`} />
+              </svg>
+            </button>
+          )}
+        </div>
+      )}
     </>
   );
 };
