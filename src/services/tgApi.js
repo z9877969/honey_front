4f40348eefc60a3c2f61561df95db67cd79f1554
z9877969 <z9877969@ukr.net>
2024-05-17 19:07:51 +0300

    updates tg credentials

--- src/services/tgApi.js
+++ src/services/tgApi.js
@@ -1,18 +1,14 @@
 import axios from 'axios';
 import { createTgMessage } from 'helpers';
-// import { totalPrice } from 'modules/cart';
-
-const TOKEN = '7176333432:AAH7wJvFkRPxLuWrPlix0yO44attqNZtwEA';
-// отримується при перевідправці повідомлення в бот GET_MY_ID
-const CHAT_ID = '-1002099110422';
+const { VITE_TG_BOT_TOKEN, VITE_TG_CHANEL_ID } = import.meta.env;
 
 const instance = axios.create({
-  baseURL: `https://api.telegram.org/bot${TOKEN}`,
+  baseURL: `https://api.telegram.org/bot${VITE_TG_BOT_TOKEN}`,
 });
 
 export const sendMessageTg = async (message) => {
   const { data } = await instance.post('/sendMessage', {
-    chat_id: CHAT_ID,
+    chat_id: VITE_TG_CHANEL_ID,
     parse_mode: 'html',
     text: createTgMessage(message),
   });
